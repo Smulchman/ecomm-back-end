959f290a386d6bc1092bfe6d1a531ff4b129b0d9
smulch <mulcahy.samuel@gmail.com>
2023-03-16 22:21:27 -0400

    set tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,7 @@ router.delete("/:id", async (req, res) => {
       },
     });
     if (!destroyedProduct) {
-      res.status(404).json({ message: "No Product found with that id!" });
+      res.status(404).json({ message: "No product found with that id!" });
       return;
     }
     res.status(200).json(destroyedProduct);
