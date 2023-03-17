17bf5b6d2ca686fcb68b2ca285e7a57c9539dae3
smulch <mulcahy.samuel@gmail.com>
2023-03-16 22:23:05 -0400

    edited comments in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 router.get("/", async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  // includes its associated Product data
   try {
     const tagData = await Tag.findAll({
       include: [{ model: Product }],
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // includes its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -67,7 +67,7 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete on tag by its `id` value
+  // delete tag by its `id` value
   try {
     const destroyedTag = await Tag.destroy({
       where: {
